64a93d8798c7a4825f5626e150291f7ded33e162
Moroswag21 <esnchez@frba.utn.edu.ar>
2021-10-13 12:06:22 -0300

    main

--- Trabajo integrador/js.js
+++
@@ -1,4 +0,0 @@
-function comprarTickets()
-{
-    document.location("G:\One Drive\OneDrive\Escritorio\UTN\FULL STACK\Repositorios\HTTSCSSJVBTSP\Trabajo integrador\Trabajo integrador JS\index.html");
-}
